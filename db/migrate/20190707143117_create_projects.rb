516e4a2a4837e3a535f2adf73a83138c149f8644
ramonsantos <ramonsantos.pe@gmail.com>
2019-07-07 12:31:25 -0300

    Add Project CRUD

---
+++ db/migrate/20190707143117_create_projects.rb
@@ -0,0 +1,11 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :name
+      t.date :start_date
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
