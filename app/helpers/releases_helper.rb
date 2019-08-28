08764ef0dea5145e7b99f227e54ecaf9082a9972
ramonsantos <ramonsantos.pe@gmail.com>
2019-08-28 20:58:48 -0300

    Remove temporary CSS

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ReleasesHelper
-  def user_stories_by_situation(user_stories, situation)
-    user_stories.select { |us| us.situation == situation }
+  def user_stories_by_situation(situation)
+    @user_stories.select { |us| us.situation == situation }
   end
 end
